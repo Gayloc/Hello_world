e3978969ddf065d5eb0852ff44c83ca50b5bb894
Gayloc <gu18647403665@outlook.com>
2024-03-27 20:27:45 +0800

    feat: 添加根据选择输出多种消息的功能

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("输入1或者2");
+        Scanner scan=new Scanner(System.in);
+        int selection=scan.nextInt();
+        switch (selection) {
+            case 1->System.out.println("Hello world");
+            case 2->System.out.println("Nice world");
+            default -> System.out.println("Goodbye world");
+        }
     }
 }
